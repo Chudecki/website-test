27f8f53701ec9d0aa037a4fc836254b6e35fb131
Jakub Chudecki <jjchudecki@gmail.com>
2026-04-08 23:25:44 +0100

    got the button to work. displays text onclick.

--- website/app.py
+++ website/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+list_with_data=[]
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -12,6 +14,8 @@ def handle_form():
         return "", 204
     data = request.get_json(force=True)
     return jsonify({ "message": "You clicked yes!" })
+    list_with_data.append(data)
+    return (list_with_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
