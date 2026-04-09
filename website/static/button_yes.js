8ec5b5773b17fad3a75b86855e64cb847da2b1ab
Jakub Chudecki <jjchudecki@gmail.com>
2026-04-09 18:29:45 +0100

    added JS code to make submiting the form send data to the flask for later proccessing

--- website/static/button_yes.js
+++ website/static/button_yes.js
@@ -1,11 +1,27 @@
-function yes() {
+function form_submit(form_event){
+    form_event.preventDefault()
     fetch("/submit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ answer:'yes'})
+        body: JSON.stringify({
+
+            name: document.getElementById("name").value,
+            address: document.getElementById("address").value,
+            birthdate: document.getElementById("birthdate").value,
+            telephone: document.getElementById("telephone").value,
+            city: document.getElementById("city").value,
+            state: document.getElementById("state").value,
+            zip: document.getElementById("zip").value,
+            what_job: document.getElementById("what_job").value,
+            what_do: document.getElementById("what_do").value,
+            previous: document.getElementById("previous").value,
+            explain_why_fired:document.getElementById("explain_why_fired").value,
+        })
     })
     .then(res => res.json())
     .then(data => {
         document.getElementById("demo").innerHTML = data.message;
     });
 }
+
+
