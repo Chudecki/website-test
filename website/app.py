bddbb322d15003235dbb0c38fab95433ef488054
Jakub Chudecki <jjchudecki@gmail.com>
2026-05-20 21:10:19 +0100

    made it so the name of the user will be displayed with the date for the json file saving

--- website/app.py
+++ website/app.py
@@ -4,17 +4,16 @@ import json
 from datetime import datetime
 
 def json_data_handling(data):
+    name_for_filepath = data["name"]
+    
     folder_path = 'website/json_user_data'
-
 
     os.makedirs(folder_path, exist_ok=True)
 
-  
     date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     name = "test_name"
 
-
-    file_path = os.path.join(folder_path, f"{date}_{name}.json")
+    file_path = os.path.join(folder_path, f"{name_for_filepath}_{date}.json")
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
 
@@ -31,7 +30,7 @@ def handle_form():
 
     try:
         data = request.get_json(force=True)
-        print("DATA:", data)
+        print("data recieved")
 
         if not data:
             return jsonify({"message": "No data received"}), 400
